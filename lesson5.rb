5fd9bc2558e4362d3ffb00c3b495c428d749f2cc
佐々木　大器 <DD@yamadamanabunoMacBook-Pro.local>
2020-01-06 15:36:48 +0900

    [start] 2-6

---
+++ lesson5.rb
@@ -0,0 +1,15 @@
+puts 100
+puts 100+5
+
+puts "rubyを学習"
+name = "鈴木さん"
+puts "Hello"+name
+puts "こんにちは#{name}"
+puts #{name}
+puts "#{name}"
+
+$g = "global"
+p $g
+
+names = ["Perl","Ruby","Python","Swift"]
+puts names[1]
