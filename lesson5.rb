20f65d4fda2f6f298619fa67b97ff874f86a42c0
佐々木　大器 <DD@yamadamanabunoMacBook-Pro.local>
2020-01-06 16:07:43 +0900

    [finish] 2-6

--- lesson5.rb
+++ lesson5.rb
@@ -13,3 +13,17 @@ p $g
 
 names = ["Perl","Ruby","Python","Swift"]
 puts names[1]
+
+tall = {"太郎"=>185,"二郎"=>170,"花子"=>150}
+puts tall["太郎"]
+
+tall = {:太郎=>185,:二郎=>170,:花子=>150}
+puts tall[:花子]
+puts tall["花子"]
+
+
+subjects = ["国語","算数","理科","社会"]
+puts subjects[2]
+
+subject = {"1"=>"国語","2"=>"算数","3"=>"理科","4"=>"社会"}
+puts subject["1"]
